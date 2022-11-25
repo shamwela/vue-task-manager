d43a05a9bac8fade13ecf16c48589e679804ef48
Sha Mwe La <shamwela2002@gmail.com>
2022-11-25 23:07:07 +0630

    Improve routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import Navigation from '@/components/NavigationBar.vue'
+import Navigation from '@/components/TheNavigation.vue'
 import '@/assets/global.css'
 </script>
 
 <template>
   <Navigation />
-  <RouterView />
+  <router-view></router-view>
 </template>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav class="flex gap-x-8">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
